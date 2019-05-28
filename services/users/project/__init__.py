a3d7562228bddf032fb527797d1ca14afa6c3815
John Bobo <john.bobo@ltxsolutions.com>
2019-05-28 15:08:51 -0400

    made model for users

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,7 @@ import sys
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -15,6 +16,23 @@ api = Api(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    aactive = db.Column(db.Boolean(), defualt=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 class UsersPing(Resource):
     def get(self):
