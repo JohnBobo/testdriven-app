58f34e37605ce923a9a3fe0e474e0e3edd529e79
John Bobo <john.bobo@ltxsolutions.com>
2019-05-31 14:04:09 -0400

    added cli command for testing

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,4 +1,6 @@
 # services/users/manage.py
+import sys
+import unittest
 
 
 from flask.cli import FlaskGroup
@@ -15,6 +17,14 @@ def recreate_db():
     df.create_all()
     db.session.commit()
 
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccesful():
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
