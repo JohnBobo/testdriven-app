ce463365fc2b3722a4f4a382bbe36022bddc0f45
John Bobo <john.bobo@ltxsolutions.com>
2019-05-31 14:09:43 -0400

    fixed typo

--- services/users/manage.py
+++ services/users/manage.py
@@ -22,7 +22,7 @@ def test():
     """Runs the tests without code coverage"""
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccesful():
+    if result.wasSuccessful():
         return 0
     sys.exit(result)
 
